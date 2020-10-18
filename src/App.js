37f7af16c6c8798659de227aa44d07c087eece43
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-18 10:36:15 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,12 +16,33 @@ const Player = () => (
 const App = () => {
 	return (
 		<MusicPlayerProvider>
+
+			<div>
+				<nav className = 'navbar navbar-expand navbar-dark bg-dark'>
+					<div className = 'navbar-nav mr-auto'>
+						<li className = 'nav-item'>
+							<Link to = {'/'} className = 'nav-link'>
+									Home
+							</Link>
+						</li>
+
+						<li className = 'nav-item'>
+							<Link to = {'/musicplayer'} className = 'nav-link'>
+									Music Player
+							</Link>
+						</li>
+
+					</div>
+				</nav>
+			</div>
+
 			<div className='container mt-3'>
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/musicplayer' component={Player} />
 				</Switch>
 			</div>
+
 		</MusicPlayerProvider>
 	);
 };
